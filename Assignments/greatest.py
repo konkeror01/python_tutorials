0a48094df156992ac90ca8e97da1e94314b26a70
konkeror01 <konkeror01@gmail.com>
2021-08-17 18:14:52 +0200

    Started with Chapter 04 - LMAF P1 Completed

---
+++ Assignments/greatest.py
@@ -0,0 +1,5 @@
+def greatest_number(a, b, c, d):
+    return max(a, b, c, d)
+
+
+print(greatest_number(2, 3, 4, 5))
---
+++ chapter_04/list_methods_functions_p1.py
@@ -0,0 +1,20 @@
+# List Methods and Functions Part 1
+
+data = ["kaustubh", "albert", "mark", "kaustubh", "krishna", "alan"]
+
+# Methods
+# .count() method
+# Meaning -> It counts the no. of times a particular element occurs in a list.
+print(data.count("minal"))
+
+# .sort() method
+# It sorts all the elements of the list in an alphabetical order or numerical order permanently
+# print(data)
+# data.sort()
+# print(data)
+
+# Function
+# sorted() function
+# # It sorts all the elements of the list in an alphabetical order or numerical order temporarily
+print(sorted(data))
+print(data)
